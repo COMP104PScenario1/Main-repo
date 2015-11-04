14a694f5beee1b135cf0655a8f3bde78a5ff4a2e
Robert Soultanaev <robert.soultanaev@hotmail.com>
2015-11-04 19:53:03 +0000

    Added signup page

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,4 @@
 class User < ActiveRecord::Base
 	has_many :listelements
+	has_secure_password
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
 	get 'todolist/:username' => 'todolist#show'
+	
+	get 'signup' => 'users#new'
+	resources :users
+	
+	
+	
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
