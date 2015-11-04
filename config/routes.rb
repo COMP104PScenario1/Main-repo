d728247bd6247931e718b0b54c346c23a9fba4a4
Robert Soultanaev <robert.soultanaev@hotmail.com>
2015-11-04 12:14:37 +0000

    Changed accessing todo list with id to accessing it with username

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-	get 'todolist/:id' => 'todolist#show'
+	get 'todolist/:username' => 'todolist#show'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
